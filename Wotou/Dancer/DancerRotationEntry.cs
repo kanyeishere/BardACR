9affa4c4f092aa27fcfc170a1f9d7046def4bb4b
Yiwei <kanyeishere@gmail.com>
2024-10-27 08:13:07 +1100

    improve ui

--- Wotou/Dancer/DancerRotationEntry.cs
+++ Wotou/Dancer/DancerRotationEntry.cs
@@ -147,7 +147,7 @@ public class DancerRotationEntry : IRotationEntry
         for (var i = 1; i < PartyHelper.Party.Count; i++)
         {
             var index = i;
-            DancePartnerPanel?.AddHotkey("切换舞伴" + PartyHelper.Party[i].Name, new ClosedPositionHotkeyResolver(index));
+            DancePartnerPanel?.AddHotkey("切换舞伴: " + PartyHelper.Party[i].Name, new ClosedPositionHotkeyResolver(index));
         }
     }
 
