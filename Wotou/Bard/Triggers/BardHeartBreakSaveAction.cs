2357cc5fd0708d7ffe53574742e42ff51f93f028
Yiwei <kanyeishere@gmail.com>
2024-11-08 21:53:43 +1100

    修改碎心箭层数描述

--- Wotou/Bard/Triggers/BardHeartBreakSaveAction.cs
+++ Wotou/Bard/Triggers/BardHeartBreakSaveAction.cs
@@ -21,6 +21,10 @@ public class BardHeartBreakSaveAction : ITriggerAction, ITriggerBase
     public bool Draw()
     {
         ImGui.Text("这个数值修改之后，战斗重新开始时会自动重置为0");
+        ImGui.Text("注意，此数值修改会在团辅爆发期也生效" +
+                   "\n但是会在以下两种情况下无效：" +
+                   "\n - 当你打开攒碎心箭QT后，会自动在团辅技能CD还剩28秒以后，不再用碎心箭，此处修改会在这段时间内失效" +
+                   "\n - 当碎心箭层数满了之后，为了防止溢出，会自动使用碎心箭");
         ImGuiHelper.LeftInputFloat("碎心箭保留层数", ref this.HeartBreakSaveStack, 0f, 3f);
         return true;
     }
