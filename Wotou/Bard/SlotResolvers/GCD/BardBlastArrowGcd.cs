16913dae41e47333cfded6e0bea0212d1c908846
Yiwei <kanyeishere@gmail.com>
2024-12-09 17:26:34 +1100

    fix

--- Wotou/Bard/SlotResolvers/GCD/BardBlastArrowGcd.cs
+++ Wotou/Bard/SlotResolvers/GCD/BardBlastArrowGcd.cs
@@ -16,12 +16,14 @@ public class BardBlastArrowGcd : ISlotResolver
     private const uint StormBiteDot = BardDefinesData.Buffs.Stormbite;
     private const uint VenomousBiteDot = BardDefinesData.Buffs.VenomousBite;
     private const uint WindBiteDot = BardDefinesData.Buffs.Windbite;
-
+    private const uint HawkEyeBuff = BardDefinesData.Buffs.HawksEye;
+    private const uint BarrageBuff = BardDefinesData.Buffs.Barrage;
 
 
     public int Check()
     {
         var target = Core.Me.GetCurrTarget();
+        
         // 有鹰眼Buff时，且马上要续毒时，不使用爆炸箭
         if (Core.Me.HasLocalPlayerAura(HawksEye) &&
             BardRotationEntry.QT.GetQt("DOT") &&
@@ -31,6 +33,13 @@ public class BardBlastArrowGcd : ISlotResolver
              (target.HasLocalPlayerAura(VenomousBiteDot) && !target.HasMyAuraWithTimeleft(VenomousBiteDot, 8000)) ||
              (target.HasLocalPlayerAura(WindBiteDot) && !target.HasMyAuraWithTimeleft(WindBiteDot, 8000))))
             return -1;
+        
+        // 有鹰眼Buff或弩炮Buff时，且目标dot不全时，不使用爆炸箭
+        if ((Core.Me.HasAura(HawkEyeBuff) || Core.Me.HasAura(BarrageBuff)) &&
+            ((!target.HasLocalPlayerAura(CausticBiteDot) && !target.HasLocalPlayerAura(VenomousBiteDot)) ||
+             (!target.HasLocalPlayerAura(WindBiteDot) && !target.HasLocalPlayerAura(StormBiteDot))))
+            return -25;
+        
         // 有爆炸箭Buff时，直接使用爆炸箭
         if (Core.Me.HasLocalPlayerAura(BlastArrowReady))
             return 1;
