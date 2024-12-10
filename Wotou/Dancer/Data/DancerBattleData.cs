920325c3404dd9f5d430e52e6c3cb51d6a7f87f7
Yiwei <kanyeishere@gmail.com>
2024-12-11 00:20:49 +1100

    fix

--- Wotou/Dancer/Data/DancerBattleData.cs
+++ Wotou/Dancer/Data/DancerBattleData.cs
@@ -7,4 +7,5 @@ public class DancerBattleData
     public int TechnicalStepCount = 0;
     public int DanceOfTheDawnCount = 0;
     public bool HotkeyUseHighPrioritySlot = false; // 热键使用高优先级队列
+    public int LastWarningTime = 0;
 }
