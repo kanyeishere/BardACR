42c32396fae3bea7ec6f349d883fffacfd359d2f
Yiwei <kanyeishere@gmail.com>
2024-12-20 20:22:46 +1100

    uo

--- Wotou/Bard/BardRotationEntry.cs
+++ Wotou/Bard/BardRotationEntry.cs
@@ -201,7 +201,7 @@ public class BardRotationEntry : IRotationEntry
         // 添加快捷按钮 (带技能图标)
         QT.AddHotkey("防击退", new MyNormalSpellHotKeyResolver(BardDefinesData.Spells.ArmsLength, 
             SpellTargetType.Target));
-        QT.AddHotkey("续毒", new MyNormalSpellHotKeyResolver(BardDefinesData.Spells.IronJaws, 
+        QT.AddHotkey("续毒", new IronJawsHotkeyResolver(BardDefinesData.Spells.IronJaws, 
             SpellTargetType.Target));
         QT.AddHotkey("内丹",
             new MyNormalSpellHotKeyResolver(BardDefinesData.Spells.SecondWind, SpellTargetType.Target));
---
+++ Wotou/Bard/Utility/IronJawsHotkeyResolver.cs
@@ -0,0 +1,66 @@
+using System.Numerics;
+using AEAssist;
+using AEAssist.CombatRoutine;
+using AEAssist.CombatRoutine.Module;
+using AEAssist.CombatRoutine.View.JobView;
+using AEAssist.Helper;
+using AEAssist.MemoryApi;
+using Dalamud.Interface.Textures.TextureWraps;
+using ImGuiNET;
+using Wotou.Bard.Data;
+
+namespace Wotou.Bard.Utility;
+
+public class IronJawsHotkeyResolver: IHotkeyResolver
+{
+    public uint SpellId;
+    public SpellTargetType TargetType;
+    
+    public IronJawsHotkeyResolver(uint spellId, SpellTargetType targetType) 
+    {
+        this.SpellId = spellId;
+        this.TargetType = targetType;
+    }
+    
+    public void Draw(Vector2 size)
+    {
+        uint id = Core.Resolve<MemApiSpell>().CheckActionChange(SpellId);
+        Vector2 size1 = size * 0.8f;
+        ImGui.SetCursorPos(size * 0.1f);
+        IDalamudTextureWrap textureWrap;
+        if (!Core.Resolve<MemApiIcon>().GetActionTexture(id, out textureWrap))
+            return;
+        ImGui.Image(textureWrap.ImGuiHandle, size1);
+    }
+
+    public void DrawExternal(Vector2 size, bool isActive)
+    {
+        SpellHelper.DrawSpellInfo(Core.Resolve<MemApiSpell>().CheckActionChange(this.SpellId).GetSpell(), size, isActive);
+    }
+
+    public int Check()
+    {
+        return 0;
+    }
+
+
+    public new void Run()
+    {
+        Spell spell = Core.Resolve<MemApiSpell>().CheckActionChange(this.SpellId).GetSpell(this.TargetType);
+        if (!BardBattleData.Instance.HotkeyUseHighPrioritySlot)
+        {
+            if (AI.Instance.BattleData.NextSlot == null)
+                AI.Instance.BattleData.NextSlot = new Slot();
+            AI.Instance.BattleData.NextSlot.Add(spell);
+        }
+        else
+        {
+            Slot slot = new Slot();
+            slot.Add(spell);
+            if (spell.IsAbility())
+                AI.Instance.BattleData.HighPrioritySlots_OffGCD.Enqueue(slot);
+            else
+                AI.Instance.BattleData.HighPrioritySlots_GCD.Enqueue(slot);
+        }
+    }
+}
