8d9209cd255e529d9d9b6763bc3e359e9fff776c
Yiwei <kanyeishere@gmail.com>
2024-12-30 14:59:07 +1100

    fix

--- Wotou/Bard/Utility/BardCommandWindow.cs
+++ Wotou/Bard/Utility/BardCommandWindow.cs
@@ -95,7 +95,6 @@ public class BardCommandWindow
         {
             BardSettings.Instance.IsOpenCommandWindow = false;
             BardSettings.Instance.Save();
-            DancerSettings.Instance.Save();
         }
 
         ImGui.End();
