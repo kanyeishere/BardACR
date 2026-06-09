08267f506ac66d918102c9157dba54496cdc91ec
Wotou <40509435+kanyeishere@users.noreply.github.com>
2026-06-09 19:51:00 +1000

    fix (#71)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- Dancer/DancerRotationEntry.cs
+++ Dancer/DancerRotationEntry.cs
@@ -349,7 +349,7 @@ public class DancerRotationEntry : IRotationEntry
             if (DancerSettings.Instance.OpenerType == DancerOpenerType.StandardStep)
                 UiHelper.RightInputInt("倒计时提前使用小舞", ref DancerSettings.Instance.OpenerStandardStepTime, 5500, 15000, "(毫秒)");
             else
-                UiHelper.RightInputInt("倒计时提前使用大舞", ref DancerSettings.Instance.OpenerTechnicalStepTime, 6500, 15000, "(毫秒)");
+                UiHelper.RightInputInt("倒计时提前使用大舞", ref DancerSettings.Instance.OpenerTechnicalStepTime, 5500, 15000, "(毫秒)");
             ImGui.Separator();
             UiHelper.RightInputInt("开战前使用舞步结束", ref DancerSettings.Instance.OpenerTime, 0, 1000, "(毫秒)");
             ImGui.Separator();
