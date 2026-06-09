f119c5a52cc782c5b5b7c80435686d344f0a03e1
Yiwei <kanyeishere@gmail.com>
2026-06-09 19:04:45 +1000

    大舞起手

--- Dancer/Opener/DNCTechOpener100.cs
+++ Dancer/Opener/DNCTechOpener100.cs
@@ -64,6 +64,8 @@ namespace Wotou.Dancer.Opener
 
         private static void Step0(Slot slot)
         {
+            slot.Add(DancerDefinesData.Spells.Devilment.GetSpell());
+            slot.Add(DancerDefinesData.Spells.Tillana.GetSpell());
             slot.Add(DancerDefinesData.Spells.Flourish.GetSpell());
         }
     }
